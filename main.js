b324b582e76c3f26df0265b3a476338dc2a35373
ItsJady-Taken <davidkhun2004@gmail.com>
2024-10-08 14:07:59 +0700

    building binary search trees

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-class BinarySearchTree {
-    constructor(key = null, value = null, parent = null) {}
+class Node {
+      constructor(data) {
+            this.data = data; // root node
+            this.left = null; // pointer to left node
+            this.right = null; // pointer to right node
+      }
+}
+
+class tree {
+      constructor() {
+            this.root = null; // root node of the tree
+      }
+      // insert data in the tree
 }
